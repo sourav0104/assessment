f28b03dd3550f2b5ecc064c816c08d5bb1d9f14b
sourav0104 <sbsourav0104@gmail.com>
2022-10-29 13:42:09 +0530

    updated file

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import $ from "jquery";
 import "./Style.css";
 
 const Card = () => {
+
+   const [cards, setCards] = useState([]);
+   
   var userDetails = JSON.parse(localStorage.getItem("userDetails") || "[]");
   console.log(userDetails, "res");
 
   const removeData = () => {
-    localStorage.removeItem(userDetails);
+    localStorage.removeItem("userDetails");
+    window.location.reload();
   };
   // $(".hideclass").hide();
   return (
     <>
       <div className="flex">
-        {userDetails.map((item, key) => {
+        {userDetails.map((item, i) => {
           return (
-            <div className="container">
+            <div className="container" key={i}>
               <div className="card">
                 <div className="card-body">
+                  <p>Card No: {i+1}</p>
                   <p>Name: {item.name}</p>
                   <p className="hideclass">Phone: {item.phone}</p>
                   <p className="hideclass">Address: {item.address}</p>
